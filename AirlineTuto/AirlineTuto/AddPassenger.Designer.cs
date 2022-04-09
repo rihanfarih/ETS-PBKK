057b66fcb797089ab968f31c8b3d2a634f8df80a
rihanfarih <55140308+rihanfarih@users.noreply.github.com>
2022-04-09 22:15:42 +0700

    test

--- AirlineTuto/AirlineTuto/AddPassenger.Designer.cs
+++ AirlineTuto/AirlineTuto/AddPassenger.Designer.cs
@@ -413,7 +413,7 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.PhoneTb);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AddPassenger";
